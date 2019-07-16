7fb64b69c28045d951f3802a9fda1a0fc597f4fc
Abhishek Sharma <sharm271@data.cs.purdue.edu>
2019-07-16 10:50:04 -0400

    revising run.py

--- run.py
+++ run.py
@@ -10,11 +10,11 @@ Options:
     -h --help                           show this screen.
     --train-file=<file>                 train_corpus [default: ../data/snli_train.txt]
     --dev-file=<file>                   dev_corpus [default: ../data/snli_dev.txt]
-    --word-embeddings=<file>            word_vecs [default: ../data/wiki-news-300d-1M.txt]
     --vocab-file=<file>                 vocab json [default: vocab.json]
+    --word-embeddings=<file>            word_vecs [default: ../data/wiki-news-300d-1M.txt]
     --max-epoch=<int>                   max epoch [default: 15]
+    --patience=<int>                    wait for how many epochs to exit training [default: 2]
     --batch-size=<int>                  batch size [default: 16]
-    --log-every=<int>                   log every [default: 10]
     --embed-size=<int>                  word embed_dim [default: 300]
     --hidden-size=<int>                 hidden dim [default: 256]
     --num-layers=<int>                  number of layers [default: 1]
@@ -32,14 +32,13 @@ import torch
 
 from docopt import docopt
 
-from utils import readCorpus
 from utils import loadEmbeddings
 from utils import extractPairCorpus
 from utils import batch_iter
-from utils import save
 from vocab import Vocab
 
 from neural_model import NeuralModel
+from nli_model import NLIModel
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -54,7 +53,6 @@ def train_lg_model(args, vocab, embeddings, train_data, label):
     """
     train_batch_size = int(args['--batch-size'])
     clip_grad = float(args['--clip-grad'])
-    log_every = int(args['--log-every'])
     model_save_path = label + args['--save-model-to']
 
     model = NeuralModel(vocab, int(args['--embed-size']), embeddings,
@@ -65,20 +63,19 @@ def train_lg_model(args, vocab, embeddings, train_data, label):
 
     optimizer = torch.optim.Adam(model.parameters(), lr=float(args['--lr']))
 
-    train_time = begin_time = time.time()
-    train_iter = report_loss = report_hyp_words = 0
-    patience = cum_loss = cum_hyp_words = cum_examples = report_examples = 0
+    total_loss = .0
+    total_hyp_words = 0
 
+    begin_time = time.time()
     for epoch in range(int(args['--max-epoch'])):
         for prems, hyps in batch_iter(train_data, batch_size=train_batch_size, shuffle=True):
-            train_iter += 1
-            hyp_words_num_to_predict = sum(len(hyp[1:]) for hyp in hyps)
+            num_hyp_words_to_predict = sum(len(hyp[1:]) for hyp in hyps)
 
             optimizer.zero_grad()
             
             batch_size = len(prems)
             batch_loss = -model(prems, hyps).sum()
-            loss = batch_loss / hyp_words_num_to_predict
+            loss = batch_loss / num_hyp_words_to_predict
 
             loss.backward()
 
@@ -86,29 +83,21 @@ def train_lg_model(args, vocab, embeddings, train_data, label):
             optimizer.step()
             
             batch_losses_val = batch_loss.item()
-            report_loss += batch_losses_val
-            cum_loss += batch_losses_val
+            total_loss += batch_losses_val
 
-            report_hyp_words += hyp_words_num_to_predict
-            cum_hyp_words += hyp_words_num_to_predict
-            report_examples += batch_size
-            cum_examples += batch_size
-
-            if train_iter % log_every == 0:
-                print('epoch= %d, iter= %d, avg. loss= %.2f, avg. ppl= %.2f, cum. examples= %d, time elapsed= %.2f sec'
-                    %(epoch, train_iter, report_loss / report_examples, math.exp(report_loss / report_hyp_words), cum_examples, time.time() - begin_time))
-
-                train_time = time.time()
-                report_loss = report_hyp_words = report_examples = 0
-
-    save(model.state_dict(), model_save_path)
+            total_hyp_words += num_hyp_words_to_predict
+        
+        print('epoch = %d, loss = %.2f, perplexity = %.2f, time_elapsed = %.2f sec'
+            % (epoch, total_loss / total_hyp_words, 2**(total_loss / total_hyp_words), time.time() - begin_time))
+        #reset epoch progress vars
+        total_loss = .0
+        total_hyp_words = 0
 
 def train(args):
     """
     train neural models
     @param args (dict): command line args
     """
-    train_sents = readCorpus(args['--train-file'])
     vocab = Vocab.load(args['--vocab-file'])
     embeddings = loadEmbeddings(vocab, args['--word-embeddings'], device)
 
