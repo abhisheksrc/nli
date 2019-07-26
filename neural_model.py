8ed16361d5278549eed0291d703e12f64e68ad89
Abhishek Sharma <sharm271@data.cs.purdue.edu>
2019-07-26 12:46:25 -0400

    adding beam search Neural model

--- neural_model.py
+++ neural_model.py
@@ -41,36 +41,37 @@ class NeuralModel(nn.Module):
         @param hyps (list[list[str]]): batches of hypothesis (list[str])
         @return scores (torch.Tensor(batch-size, )): log-likelihod of generating the words in the hyps
         """
-        prems_lengths = [len(prem) for prem in prems]
+        prem_lengths = [len(prem) for prem in prems]
         
-        prems_padded = self.vocab.sents2Tensor(prems, device=self.device)
-        hyps_padded = self.vocab.sents2Tensor(hyps, device=self.device)
+        prem_padded = self.vocab.sents2Tensor(prems, device=self.device)
+        hyp_padded = self.vocab.sents2Tensor(hyps, device=self.device)
 
-        enc_hiddens, dec_init_state = self.encode(prems_padded, prems_lengths)
-        hyps_predicted = self.decode(hyps_padded, dec_init_state)
-        P = F.log_softmax(hyps_predicted, dim=-1)        
+        enc_hiddens, dec_init_state = self.encode(prem_padded, prem_lengths)
+        hyp_predicted = self.decode(hyp_padded, dec_init_state)
+        P = F.log_softmax(hyp_predicted, dim=-1)        
         
         #create mask to zero out probability for the pad tokens
-        hyps_mask = (hyps_padded != self.vocab.pad_id).float()
-        #compute cross-entropy between hyps_words and hyps_predicted_words
-        hyps_predicted_words_log_prob = torch.gather(P, dim=-1, 
-            index=hyps_padded[1:].unsqueeze(-1)).squeeze(-1) * hyps_mask[1:]
-        scores = hyps_predicted_words_log_prob.sum(dim=0)
+        hyp_mask = (hyp_padded != self.vocab.pad_id).float()
+        #compute cross-entropy between hyp_words and hyp_predicted_words
+        hyp_predicted_words_log_prob = torch.gather(P, dim=-1, 
+            index=hyp_padded[1:].unsqueeze(-1)).squeeze(-1) * hyp_mask[1:]
+        scores = hyp_predicted_words_log_prob.sum(dim=0)
         return scores
 
-    def encode(self, prems, prems_lens):
+    def encode(self, prems, prem_lens):
         """
         apply the encoder on the premises to obtain encoder hidden states
         @param prems (torch.tensor(max_prem_len, batch))
-        @param prems_lens (list[int]): list of actual lengths of the prems
+        @param prem_lens (list[int]): list of actual lengths of the prems
         @return enc_hiddens (torch.tensor(max_prem_len, batch, hidden*2)): tensor of seq of hidden outs
         @return dec_init_state (tuple(torch.tensor(batch, hidden*2), torch.tensor(batch, hidden*2))): encoders final hidden state and cell i.e. decoders initial hidden state and cell
         """
         X = self.embeddings(prems)
-        X = rnn.pack_padded_sequence(X, prems_lens)
+        X = rnn.pack_padded_sequence(X, prem_lens)
         enc_hiddens, (h_e, c_e) = self.encoder(X)
-        enc_hiddens, prems_lens_tensor = rnn.pad_packed_sequence(enc_hiddens)
+        enc_hiddens, prem_lens_tensor = rnn.pad_packed_sequence(enc_hiddens)
         batch = prems.shape[1]
+        #h_e.shape = (2, b, h)
         h_e_cat = torch.cat((h_e[0, :, :], h_e[1, :, :]), dim=1).to(self.device)
         c_e_cat = torch.cat((c_e[0, :, :], c_e[1, :, :]), dim=1).to(self.device)
         return enc_hiddens, (h_e_cat, c_e_cat)
@@ -79,7 +80,7 @@ class NeuralModel(nn.Module):
         """
         @param hyps (torch.tensor(max_hyp_len, batch))
         @param dec_init_state (tuple(torch.tensor(batch, hidden*2), torch.tensor(batch, hidden*2))): h_decoder_0, c_decoder_0
-        @return hyps_predicted (torch.tensor(max_hyp_len-1, batch, vocab)): batch of seq of hyp words
+        @return hyp_predicted (torch.tensor(max_hyp_len-1, batch, vocab)): batch of seq of hyp words
         """
         #Chop the last token from all the hyps
         hyps = hyps[:-1]
@@ -91,23 +92,81 @@ class NeuralModel(nn.Module):
         batch_size = hyps.shape[1]
 
         for y_t in torch.split(Y, split_size_or_sections=1, dim=0):
-            y_t = torch.squeeze(y_t, dim=0)#shape(1, b, e)
+            y_t = torch.squeeze(y_t, dim=0)#shape(1, b, e) -> (b, e)
             h_t, c_t = self.decoder(y_t, (h_t, c_t))
             hidden_outs.append(h_t)
 
         hidden_outs = torch.stack(hidden_outs, dim=0)
-        hyps_predicted = self.vocab_projection(hidden_outs)
-        return hyps_predicted
+        hyp_predicted = self.vocab_projection(hidden_outs)
+        return hyp_predicted
 
     def beam_search(self, prem, beam_size, max_decoding_time_step):
         """
-        given a premise generate possible hyps, from this LG model, up to the beam size
+        given a premise search possible hyps, from this LG model, up to the beam size
         @param prem (list[str]): premise
         @param beam_size (int)
         @param max_decoding_time_step (int): decode the hyp until <eos> or max decoding time step
-        @return possible_hyps (list[list[str]]): generate beam_size number of hyps
+        @return best_hyp (list[str]): best possible hyp
         """
-        #TODO
+        prems = [prem]
+        prem_lens = [len(prem)]
+        prem_padded = self.vocab.sents2Tensor(prems, device=self.device)
+
+        enc_hiddens, dec_init_state = self.encode(prem_padded, prem_lens)
+
+        t = 0
+
+        hyps = [['<start>']]
+        completed_hyps = []
+        hyp_scores = torch.zeros(len(hyps), dtype=torch.float, device=self.device)
+
+        (h_t, c_t) = dec_init_state
+        while len(completed_hyps) < beam_size and t < max_decoding_time_step:
+            y_t = torch.tensor([self.vocab[hyp[-1]] for hyp in hyps], dtype=torch.long, device=self.device)
+            y_t = self.embeddings(y_t)
+            h_t, c_t = self.decoder(y_t, (h_t, c_t))
+
+            log_p_t = F.log_softmax(self.vocab_projection(h_t), dim=-1)
+            
+            live_hyp_num = beam_size - len(completed_hyps)
+            live_hyp_scores = (hyp_scores.unsqueeze(1).expand_as(log_p_t) + log_p_t).view(-1) #shape = live_hyp_num * len(vocab)
+            top_word_scores, top_word_pos = torch.topk(live_hyp_scores, k=live_hyp_num)
+
+            prev_hyp_ids = top_word_pos / len(self.vocab)
+            hyp_word_ids = top_word_pos % len(self.vocab)
+
+            new_hyps = []
+            live_hyp_ids = []
+            new_hyp_scores = []
+
+            for prev_hyp_id, hyp_word_id, top_word_score in zip(prev_hyp_ids, hyp_word_ids, top_word_scores):
+                prev_hyp_id = prev_hyp_id.item()
+                hyp_word_id = hyp_word_id.item()
+                top_word_score = top_word_score.item()
+
+                hyp_word = self.vocab.id2word[hyp_word_id]
+                new_hyp_sent = hyps[prev_hyp_id] + [hyp_word]
+                if hyp_word == '<eos>':
+                    completed_hyps.append(new_hyp_sent[1:-1], top_word_score)
+                else:
+                    new_hyps.append(new_hyp_sent)
+                    live_hyp_ids.append(prev_hyp_id)
+                    new_hyp_scores.append(top_word_score)
+
+            hyps = new_hyps
+            
+            live_hyp_ids = torch.tensor(live_hyp_ids, dtype=torch.long, device=self.device)
+            h_t, c_t = h_t[live_hyp_ids], c_t[live_hyp_ids]
+
+            hyp_scores = torch.tensor(new_hyp_scores, dtype=torch.float, device=self.device)
+
+            t += 1
+        #end-while
+        if len(completed_hyps) == 0:
+            completed_hyps.append(hyps[0][1:], hyp_scores[0].item())
+
+        completed_hyps.sort(key=lambda (hyp, score): score, reverse=True)
+        return completed_hyps[0]
 
     def save(self, file_path):
         """
