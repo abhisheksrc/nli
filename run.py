8b7ca53f6f6eddcefa2017dc363e6c4c0d7a4afe
Abhishek Sharma <sharm271@data.cs.purdue.edu>
2019-07-26 17:53:18 -0400

    finishing adding evaluation Neural model

--- run.py
+++ run.py
@@ -4,21 +4,22 @@ run.py: run script for NLI LG Model
 Abhishek Sharma <sharm271@cs.purdue.edu>
 
 Usage:
-    run.py train --train-file=<file> --dev-file=<file> [options]
+    run.py train NLI_MODEL_PATH --train-file=<file> --dev-file=<file> [options]
 
 Options:
     -h --help                           show this screen.
     --train-file=<file>                 train_corpus
     --dev-file=<file>                   dev_corpus
+    --save-generated-hyp-to=<file>      save generated hyp [default: _lg_result.txt]              
     --vocab-file=<file>                 vocab json [default: vocab.json]
     --word-embeddings=<file>            word_vecs [default: ../data/wiki-news-300d-1M.txt]
     --max-epoch=<int>                   max epoch [default: 15]
     --patience=<int>                    wait for how many epochs to exit training [default: 2]
-    --batch-size=<int>                  batch size [default: 16]
+    --batch-size=<int>                  batch size [default: 32]
     --embed-size=<int>                  word embed_dim [default: 300]
     --hidden-size=<int>                 hidden dim [default: 256]
     --clip-grad=<float>                 grad clip [default: 5.0]
-    --lr=<float>                        learning rate [default: 0.001]
+    --lr=<float>                        learning rate [default: 1e-4]
     --dropout=<float>                   dropout rate [default: 0.3]
     --beam-size=<int>                   beam size [default: 2]
     --max-decoding-time-step=<int>      max decoding time steps [default: 70]
@@ -35,11 +36,14 @@ from docopt import docopt
 
 from utils import loadEmbeddings
 from utils import extractPairCorpus
+from utils import extractPrems
 from utils import batch_iter
+from utils import save_generated_hyps
 from vocab import Vocab
 
 from neural_model import NeuralModel
 from nli_model import NLIModel
+from nli_train import evaluate as nli_evaluate
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -50,7 +54,9 @@ def evaluate(args, model, prems, label):
     @param model (NeuralModel): Neural Model
     @param prems (list[list[str]]): list of prems
     @param label (str): hyp label to generate
-    @return TODO
+    @return hyps (list[list[str]]): list of hyps
+    @return nli_class_loss (float): NLI classification loss
+    @return nli_class_acc (float): NLI classification accuracy
     """
     was_training = model.training
     model.eval()
@@ -58,15 +64,24 @@ def evaluate(args, model, prems, label):
     hyps = []
     with torch.no_grad():
          for prem in prems:
-            possible_hyps = model.beam_search(prem, 
+            possible_hyp = model.beam_search(prem, 
                 beam_size=int(args['--beam-size']),
-                max_decoding_time_step=int(args['max-decoding-time-step']))
-            hyps.append(possible_hyps)
+                max_decoding_time_step=int(args['--max-decoding-time-step']))
+            hyps.append(possible_hyp)
 
     if was_training:
         model.train()
 
-    #return TODO
+    nli_data = []
+    nli_model = NLIModel.load(args['NLI_MODEL_PATH'])
+    nli_model = nli_model.to(device)
+
+    for prem, hyp in zip(prems, hyps):
+        nli_data.append((prem, hyp, label))
+
+    nli_class_loss, nli_class_acc = nli_evaluate(nli_model, nli_data, batch_size=int(args['--batch-size']))
+
+    return hyps, nli_class_loss, nli_class_acc
 
 def train_lg_model(args, vocab, embeddings, train_data, label):
     """
@@ -86,10 +101,17 @@ def train_lg_model(args, vocab, embeddings, train_data, label):
                         dropout_rate=float(args['--dropout']), device=device)
     model = model.to(device)
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=float(args['--lr']))
+    init_lr = float(args['--lr'])
+    optimizer = torch.optim.Adam(model.parameters(), lr=init_lr)
 
     total_loss = .0
     total_hyp_words = 0
+
+    dev_prems = extractPrems(args['--dev-file'], specific_label=label)
+    generated_hyp_path = label + args['--save-generated-hyp-to']
+
+    hist_dev_scores = []
+    patience = 0
 
     begin_time = time.time()
     for epoch in range(int(args['--max-epoch'])):
@@ -119,6 +141,32 @@ def train_lg_model(args, vocab, embeddings, train_data, label):
         total_hyp_words = 0
 
         #perform validation
+        dev_hyps, dev_loss, dev_acc =evaluate(args, model, dev_prems, label)
+        is_better = epoch == 0 or dev_acc > max(hist_dev_scores)
+        hist_dev_scores.append(dev_acc)
+
+        if is_better:
+            #reset patience
+            patience = 0
+            #save model
+            model.save(model_save_path)
+            #save generated hyps
+            save_generated_hyps(generated_hyp_path, dev_prems, dev_hyps)
+
+        else:
+            patience += 1
+            if patience == int(args['--patience']):
+                print('finishing training: dev loss = %.2f, dev acc. = %.2f'
+                    % (dev_loss, dev_acc))
+                exit(0)
+
+        print('validation: dev loss = %.2f, dev acc. = %.2f'
+            % (dev_loss, dev_acc))
+
+        #update lr after every 2 epochs
+        lr = init_lr / 2 ** (epoch // 2)
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
 
 def train(args):
     """
