b26d19bf344692e9392f7b9df5006275b78fdd6f
Abhishek Sharma <sharm271@cuda.cs.purdue.edu>
2019-08-07 14:13:54 -0400

    transitioning from nli to sts

---
+++ src/sts_model.py
@@ -0,0 +1,158 @@
+#!/usr/bin/python
+import torch
+import torch.nn as nn
+from torch.nn.utils import rnn
+import torch.nn.functional as F
+
+from utils import sortHyps
+from model_embeddings import ModelEmbeddings
+
+class NLIModel(nn.Module):
+    """
+    Bi-LSTM encoder
+    max pooling
+    classifier
+    """
+    def __init__(self, vocab, embed_size, embeddings, hidden_size, mlp_size, dropout_rate, device):
+        """
+        @param vocab (Vocab): vocab object
+        @param embed_size (int): embedding size
+        @param embeddings (torch.tensor (len(vocab), embed_dim)): pretrained word embeddings
+        @param hidden_size (int): hidden size
+        @param mlp_size (int): mlp size
+        @param dropout_rate (float): dropout prob
+        """
+        super(NLIModel, self).__init__()
+        self.pretrained_embeddings = embeddings
+        self.embeddings = ModelEmbeddings(vocab, embed_size, self.pretrained_embeddings)
+        self.device = device
+        self.vocab = vocab
+        self.hidden_size = hidden_size
+        self.mlp_size = mlp_size
+        self.dropout_rate = dropout_rate
+
+        #self.encoder = nn.LSTM(input_size=embed_size, hidden_size=self.hidden_size, num_layers=self.num_layers, bias=True, bidirectional=True)
+        self.lstm_1 = nn.LSTM(input_size=embed_size, hidden_size=self.hidden_size, num_layers=1, bias=True, bidirectional=True)
+        self.lstm_2 = nn.LSTM(input_size=(embed_size + self.hidden_size*2), hidden_size=self.hidden_size*2, num_layers=1, bias=True, bidirectional=True)
+        self.lstm_3 = nn.LSTM(input_size=(embed_size + self.hidden_size*2 + self.hidden_size*4), hidden_size=self.hidden_size*4, num_layers=1, bias=True, bidirectional=True)
+
+        #classifier: in_features = final lstm out_size * 2 (bidirectional) * 4 (prem-hyp feature concat)
+        #           out_fearures = 3 labels
+        self.mlp_1 = nn.Linear(in_features=self.hidden_size*4*2*4, out_features=self.mlp_size)
+        self.mlp_2 = nn.Linear(in_features=self.mlp_size, out_features=self.mlp_size)
+        self.sm = nn.Linear(in_features=self.mlp_size, out_features=3)
+        self.classifier = nn.Sequential(*[self.mlp_1, nn.ReLU(), nn.Dropout(self.dropout_rate),
+                                        self.mlp_2, nn.ReLU(), nn.Dropout(self.dropout_rate),
+                                        self.sm])        
+
+    def forward(self, prems, hyps):
+        """
+        given a batch of prems and hyps, run encoder on prems and hyps
+        followed by max-pooling
+        and finally pass through the classifier
+        @param prems (list[list[str]]): batches of premise (list[str])
+        @param hyps (list[list[str]]): batches of hypothesis (list[str])
+        @return outs (torch.Tensor(batch-size, 3)): log-likelihod of 3-labels
+        """
+        prem_lengths = [len(prem) for prem in prems]
+        prem_padded = self.vocab.sents2Tensor(prems, device=self.device)
+
+        prem_enc_out = self.encode(prem_padded, prem_lengths)
+
+        #reverse sort hyps and save original lengths and index mapping:
+        #   map: indices_sorted -> indices_orig
+        hyp_lengths_orig = [len(hyp) for hyp in hyps]
+        hyp_sorted, orig_to_sorted = sortHyps(hyps)
+        hyp_lengths_sorted = [len(hyp) for hyp in hyp_sorted]
+        hyp_padded = self.vocab.sents2Tensor(hyp_sorted, device=self.device)
+
+        hyp_enc_out = self.encode(hyp_padded, hyp_lengths_sorted)
+        hyp_enc_out_seq_orig = [hyp_enc_out[:, orig_to_sorted[i], :].unsqueeze(dim=1) 
+                                    for i in range(len(hyps))]
+        hyp_enc_out_orig = torch.cat(hyp_enc_out_seq_orig, dim=1)
+
+        #max pooling and classifier
+        prem_enc_final = self.maxPool(prem_enc_out, prem_lengths)
+        hyp_enc_final = self.maxPool(hyp_enc_out_orig, hyp_lengths_orig)
+        
+        classifier_features = torch.cat([prem_enc_final, hyp_enc_final, torch.abs(prem_enc_final - hyp_enc_final), prem_enc_final * hyp_enc_final], dim=-1)
+        
+        outs = self.classifier(classifier_features)
+        return outs 
+
+    def encode(self, sents, sents_lens):
+        """
+        apply the encoder on the sentences to obtain encoder hidden states
+        @param sents (torch.tensor(max_sent_len, batch))
+        @param sents_lens (list[int]): list of actual lengths of the sents
+        @return out_layer_3 (torch.tensor(max_sent_len, batch, hidden*4*2)): 
+            the final out from the laste lstm layer
+        """
+        X = self.embeddings(sents)
+        out_layer_1 = self.run_BiLSTM(self.lstm_1, X, sents_lens)
+
+        in_layer_2 = torch.cat([X, out_layer_1], dim=-1)
+        out_layer_2 = self.run_BiLSTM(self.lstm_2, in_layer_2, sents_lens)
+
+        in_layer_3 = torch.cat([X, out_layer_1, out_layer_2], dim=-1)
+        out_layer_3 = self.run_BiLSTM(self.lstm_3, in_layer_3, sents_lens)
+
+        return out_layer_3
+
+    def run_BiLSTM(self, lstm_model, in_layer, sents_lens):
+        """
+        run BiLSTM on the input
+        @param lstm_model (nn.LSTM): LSTM model
+        @param in_layer (torch.tensor(max_sent_len, batch, embed + hidden*((2 ** i) - 2))): input layer for the model, i = layer#
+        @param sents_lens (list[int]): list of actual lengths of the sents
+        @return out_layer (torch.tensor(max_sent_len, batch, hidden*2*i)): output layer of the model, i = layer#
+        """
+        in_layer = rnn.pack_padded_sequence(in_layer, sents_lens)
+        out_layer, (h_n, c_n) = lstm_model(in_layer)
+        out_layer, sents_lens_tensor = rnn.pad_packed_sequence(out_layer)
+        return out_layer
+
+    def maxPool(self, encodings, lengths):
+        """
+        apply max pool to each encoding to extract the max element
+        @param encodings (torch.tensor(max_encoding_len, batch, hidden*2)):
+            the out sequence of encoder
+        @param lengths (list[int]): list of actual lengths of the encodings
+        @return outs_encoder_final (torch.tensor(batch, hidden*2)):
+            final out of the encoder
+        """
+        seq_max_list = []
+        for i, length in enumerate(lengths):
+            seq_i = encodings[:length, i, :]
+            seq_i_max, _ = seq_i.max(dim=0)
+            seq_i_max = torch.squeeze(seq_i_max, dim=0)
+            seq_max_list.append(seq_i_max)
+        return torch.stack(seq_max_list)
+
+    def save(self, file_path):
+        """
+        saving model to the file_path
+        """
+        params = {
+            'vocab' : self.vocab,
+            'args' : dict(embed_size=self.embeddings.embed_size, 
+                        embeddings=self.pretrained_embeddings,
+                        hidden_size=self.hidden_size, mlp_size=self.mlp_size,
+                        dropout_rate=self.dropout_rate, device=self.device),
+            'state_dict': self.state_dict()      
+        }
+        torch.save(params, file_path)
+
+    @staticmethod
+    def load(model_path):
+        """
+        load a saved neural model
+        @param model_path (str): path to model
+        @return model (NLIModel)
+        """
+        params = torch.load(model_path, map_location=lambda storage, loc: storage)
+        args = params['args']
+        model = NLIModel(vocab=params['vocab'], **args)
+        model.load_state_dict(params['state_dict'])
+
+        return model
---
+++ src/sts_train.py
@@ -0,0 +1,191 @@
+#!/usr/bin/python
+"""
+nli_train.py: Script for NLI Model
+Abhishek Sharma <sharm271@cs.purdue.edu>
+
+Usage:
+    nli_train.py train --train-file=<file> --dev-file=<file> [options]
+    nli_train.py test MODEL_PATH --test-file=<file> [options]
+
+Options:
+    -h --help                           show this screen.
+    --train-file=<file>                 train_corpus
+    --dev-file=<file>                   dev_corpus
+    --test-file=<file>                  test_corpus
+    --vocab-file=<file>                 vocab json [default: vocab.json]
+    --word-embeddings=<file>            word_vecs [default: ../data/wiki-news-300d-1M.txt]
+    --max-epoch=<int>                   max epoch [default: 15]
+    --patience=<int>                    wait for how many epochs to exit training [default: 5]
+    --batch-size=<int>                  batch size [default: 32]
+    --embed-size=<int>                  word embed_dim [default: 300]
+    --hidden-size=<int>                 hidden dim [default: 512]
+    --mlp-hidden-size=<int>             mlp hidden dim [default: 1600]
+    --num-layers=<int>                  number of layers [default: 3]
+    --lr=<float>                        learning rate [default: 2e-4]
+    --dropout=<float>                   dropout rate [default: 0.1]
+    --save-model-to=<file>              save trained model [default: nli_model.pt]
+"""
+from __future__ import division
+
+import time
+import math
+
+import torch
+import torch.nn.functional as F
+
+from docopt import docopt
+
+from utils import readCorpus
+from utils import loadEmbeddings
+from utils import extractSentLabel
+from utils import batch_iter
+from utils import labels_to_indices
+from utils import compareLabels
+from vocab import Vocab
+from nli_model import NLIModel
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def evaluate(model, data, batch_size):
+    """
+    Evaluate the model on the data
+    @param model (NLIModel): NLI Model
+    @param data (tuple(prem, hyp, label)): data returned by extractSentLabel
+    @param batch_size (int): batch size
+    @return avg_loss (float): avg. cross entropy loss on the data
+    @return avg_acc (float): avg. classification accuracy on the data
+    """
+    was_training = model.training
+    model.eval()
+
+    total_loss = .0
+    total_correct_preds = 0
+    with torch.no_grad():
+        for prems, hyps, labels in batch_iter(data, batch_size, shuffle=True, label=True):
+            labels_pred = model(prems, hyps)
+
+            P = F.log_softmax(labels_pred, dim=-1)
+            labels_indices = labels_to_indices(labels)
+            labels_indices = labels_indices.to(device) 
+            cross_entropy_loss = torch.gather(P, dim=-1,
+                index=labels_indices.unsqueeze(-1)).squeeze(-1)
+
+            batch_loss = -cross_entropy_loss.sum()
+            num_correct_preds = compareLabels(labels_pred, labels)
+
+            batch_losses_val = batch_loss.item()
+            total_loss += batch_losses_val
+            total_correct_preds += num_correct_preds
+
+    avg_loss = total_loss / len(data)
+    avg_acc = total_correct_preds / len(data)
+    
+    if was_training:
+        model.train()
+
+    return avg_loss, avg_acc
+
+def train(args):
+    """
+    train NLI model
+    @param args (dict): command line args
+    """
+    vocab = Vocab.load(args['--vocab-file'])
+    embeddings = loadEmbeddings(vocab, args['--word-embeddings'], device)
+
+    #train NLI prediction model
+    train_data = extractSentLabel(args['--train-file'])
+    dev_data = extractSentLabel(args['--dev-file'])
+
+    train_batch_size = int(args['--batch-size'])
+    dev_batch_size = int(args['--batch-size'])
+    model_save_path = args['--save-model-to']
+
+    model = NLIModel(vocab, int(args['--embed-size']), embeddings,
+                    hidden_size=int(args['--hidden-size']),
+                    mlp_size=int(args['--mlp-hidden-size']),
+                    dropout_rate=float(args['--dropout']), device=device)
+
+    model = model.train()
+    model = model.to(device)
+
+    init_lr = float(args['--lr'])
+    optimizer = torch.optim.Adam(model.parameters(), init_lr)
+
+    total_loss = .0
+    hist_dev_scores = []
+    patience = 0
+
+    begin_time = time.time()
+    for epoch in range(int(args['--max-epoch'])):
+        for prems, hyps, labels in batch_iter(train_data, batch_size=train_batch_size, shuffle=True, label=True):
+            optimizer.zero_grad()
+            
+            batch_size = len(prems)
+            labels_pred = model(prems, hyps)
+
+            P = F.log_softmax(labels_pred, dim=-1)
+            labels_indices = labels_to_indices(labels)
+            labels_indices = labels_indices.to(device) 
+            cross_entropy_loss = torch.gather(P, dim=-1,
+                index=labels_indices.unsqueeze(-1)).squeeze(-1)
+
+            batch_loss = -cross_entropy_loss.sum()
+            loss = batch_loss / batch_size
+
+            loss.backward()
+
+            optimizer.step()
+            
+            batch_losses_val = batch_loss.item()
+            total_loss += batch_losses_val
+
+        #print train loss at the end of each epoch
+        train_loss = total_loss / len(train_data)
+        print('epoch = %d: avg. loss = %.2f, time elapsed = %.2f sec'
+            % (epoch, train_loss, time.time() - begin_time))
+        total_loss = .0
+
+        #perform validation
+        dev_loss, dev_acc = evaluate(model, dev_data, dev_batch_size)
+        is_better = epoch == 0 or dev_acc > max(hist_dev_scores)
+        hist_dev_scores.append(dev_acc)
+
+        if is_better:
+            #reset patience
+            patience = 0
+            #save model
+            model.save(model_save_path)
+
+        else:
+            patience += 1
+            if patience == int(args['--patience']):
+                print('finishing training: dev loss = %.2f, dev acc. = %.2f'
+                    % (dev_loss, dev_acc))
+                exit(0)
+
+        print('validation: dev loss = %.2f, dev acc. = %.2f'
+            % (dev_loss, dev_acc))
+        
+        #update lr after every 2 epochs
+        lr = init_lr / 2 ** (epoch // 2)
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+
+def test(args):
+    """
+    test NLI model
+    @param args (dict): command line args
+    """
+    test_data = extractSentLabel(args['--test-file'])
+    model = NLIModel.load(args['MODEL_PATH'])
+    model = model.to(device)
+    test_loss, test_acc = evaluate(model, test_data, batch_size=int(args['--batch-size']))
+    print('final test accuracy= %.4f' %(test_acc))
+
+if __name__ == "__main__":
+    args = docopt(__doc__)
+    if args['train']:
+        train(args)
+    elif args['test']:
+        test(args)
